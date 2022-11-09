1bec0346f3e311dd47262e0f3ef68f677ddb3df6
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-09 19:53:23 +0600

    data is loding successfully

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   try {
-    const productCollection = client.db('eagleEngineering').collection('services');
+    const serviceCollection = client.db('eagleEngineering').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
 
   }
   finally {
