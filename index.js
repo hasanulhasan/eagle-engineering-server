bf69393e9366e4b7f3065d7e53da013e5aebfd07
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-09 19:47:37 +0600

    database connceted

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5001;
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjh2ngr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const productCollection = client.db('eagleEngineering').collection('services');
+
+  }
+  finally {
+
+  }
+}
+run().catch(e => console.error(e))
 
 app.get('/', (req, res) => {
   res.send('Eagle server is running');
