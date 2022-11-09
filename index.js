f06ffa29cc71105713d7bfb12b99740fb9e5bd72
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-09 21:39:58 +0600

    new get method added for service all

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5001;
 
 //middle wares
@@ -16,12 +16,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     const serviceCollection = client.db('eagleEngineering').collection('services');
+    //this is used for service page
+    app.get('/servicesall', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
+    //this is used for home service with limit
     app.get('/services', async (req, res) => {
       const query = {}
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     })
 
   }
